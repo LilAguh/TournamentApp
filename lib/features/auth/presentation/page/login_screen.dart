6460411017a5b916db6268ebb81c291ab29a23e1
LilAguh <aguezeochoa@gmail.com>
2025-05-01 20:05:16 -0300

    Se crea el login de la aplicacion

--- lib/features/auth/presentation/page/login_screen.dart
+++ lib/features/auth/presentation/page/login_screen.dart
@@ -1,45 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_card_swiper/flutter_card_swiper.dart';
-
-class Example extends StatelessWidget {
-  List<Container> cards = [
-    Container(
-      alignment: Alignment.center,
-      child: const Text('HOLA BELEN'),
-      color: Colors.blue,
-    ),
-    Container(
-      alignment: Alignment.center,
-      child: const Text('2'),
-      color: Colors.red,
-    ),
-    Container(
-      alignment: Alignment.center,
-      child: const Text('3'),
-      color: Colors.purple,
-    ),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Flexible(
-        child: CardSwiper(
-          cardsCount: cards.length,
-          cardBuilder:
-              (context, index, percentThresholdX, percentThresholdY) =>
-                  cards[index],
-        ),
-      ),
-    );
-  }
-}
+import 'package:go_router/go_router.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Example();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        backgroundColor: Colors.blue[400],
+      ),
+
+      // Centrado
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const TextField(decoration: InputDecoration(labelText: 'Email')),
+              const SizedBox(height: 16),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(labelText: 'Contraseña'),
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  // lógica de login futura
+                },
+                child: const Text('Ingresar'),
+              ),
+              const SizedBox(height: 16),
+              TextButton(
+                onPressed: () {
+                  context.go('/signin');
+                },
+                child: const Text('¿No tenés cuenta? Registrate'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
