051e3f74503350523586cfaa5a7267697a049b36
LilAguh <aguezeochoa@gmail.com>
2025-05-06 18:12:50 -0300

    se elimina carpeta repetida dentro de carpeta

---
+++ lib/features/home/presentation/page/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HOME'),
+        backgroundColor: Colors.green[400],
+      ),
+    );
+  }
+}
