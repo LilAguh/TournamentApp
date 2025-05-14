21ffe5e9a3100c6f33e863c468241bfcd23db4b7
LilAguh <aguezeochoa@gmail.com>
2025-05-14 02:35:22 -0300

    se añade el estado de logout a profile

--- lib/features/profile/presentation/page/profile_screen.dart
+++ lib/features/profile/presentation/page/profile_screen.dart
@@ -1,41 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tournament_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:tournament_app/features/auth/presentation/bloc/auth_event.dart';
+import 'package:tournament_app/features/auth/presentation/bloc/auth_state.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('PROFILE'),
-        backgroundColor: Colors.green[400],
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  context.go('/auth-welcome');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red[400], // Color de fondo
-                  foregroundColor: Colors.white, // Color del texto
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24,
-                    vertical: 12,
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthInitial) {
+          print('🧼 estado: sesión eliminada');
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('PROFILE'),
+          backgroundColor: Colors.green[400],
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<AuthBloc>().add(LogoutRequested());
+                    context.go('/auth-welcome');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red[400],
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 12,
+                    ),
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  textStyle: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  child: const Text('Cerrar sesión'),
                 ),
-                child: const Text('Cerrar sesión'),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
