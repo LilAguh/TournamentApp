da46586fece8a07bf6ec9fff22603cfbb0ece70a
LilAguh <aguezeochoa@gmail.com>
2025-05-27 13:55:28 -0300

    se utiliza el boton de registrar para llamar a los paises

--- lib/features/auth/presentation/page/auth_welcome_screen.dart
+++ lib/features/auth/presentation/page/auth_welcome_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tournament_app/features/country/presentation/bloc/country_bloc.dart';
+import 'package:tournament_app/features/country/presentation/bloc/country_event.dart';
+import 'package:tournament_app/features/country/presentation/bloc/country_state.dart';
 
-class AuthWelcome extends StatelessWidget {
+class AuthWelcome extends StatefulWidget {
   const AuthWelcome({super.key});
+
+  @override
+  State<AuthWelcome> createState() => _AuthWelcome();
+}
+
+class _AuthWelcome extends State<AuthWelcome> {
+  @override
+  void initState() {
+    super.initState();
+    // context.read<CountryBloc>().add(GetCountryRequested());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,25 +29,35 @@ class AuthWelcome extends StatelessWidget {
 
       // Centrado
       body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  context.go('/login');
-                },
-                child: const Text('Iniciar sesion'),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  context.go('/register-method');
-                },
-                child: const Text('Crear nuevo usuario'),
-              ),
-            ],
+        child: BlocListener<CountryBloc, CountryState>(
+          listener: (context, state) {
+            if (state is CountrySuccess) {
+              Future.microtask(() {
+                context.go('/register-method');
+              });
+            }
+          },
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    context.go('/login');
+                  },
+                  child: const Text('Iniciar sesion'),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    final bloc = context.read<CountryBloc>();
+                    bloc.add(GetCountryRequested());
+                  },
+                  child: const Text('Crear nuevo usuario'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
