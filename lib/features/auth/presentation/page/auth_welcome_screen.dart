8a932943600f40f1229be73b8d243870aeb14ffc
LilAguh <aguezeochoa@gmail.com>
2025-05-27 15:51:29 -0300

    se instala ip_country_lookup y se verifica que funciona

--- lib/features/auth/presentation/page/auth_welcome_screen.dart
+++ lib/features/auth/presentation/page/auth_welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ip_country_lookup/ip_country_lookup.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:ip_country_lookup/models/ip_country_data_model.dart';
 import 'package:tournament_app/features/country/presentation/bloc/country_bloc.dart';
 import 'package:tournament_app/features/country/presentation/bloc/country_event.dart';
 import 'package:tournament_app/features/country/presentation/bloc/country_state.dart';
@@ -13,6 +15,11 @@ class AuthWelcome extends StatefulWidget {
 }
 
 class _AuthWelcome extends State<AuthWelcome> {
+  bool isDataLoaded = false;
+  bool isLoading = false;
+  IpCountryData? countryData;
+  String? usersPublicIpAddress;
+
   @override
   void initState() {
     super.initState();
@@ -37,28 +44,59 @@ class _AuthWelcome extends State<AuthWelcome> {
               });
             }
           },
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    context.go('/login');
-                  },
-                  child: const Text('Iniciar sesion'),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    final bloc = context.read<CountryBloc>();
-                    bloc.add(GetCountryRequested());
-                  },
-                  child: const Text('Crear nuevo usuario'),
-                ),
-              ],
-            ),
-          ),
+          child:
+              isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      isDataLoaded
+                          ? Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 44),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "name: ${countryData!.country_name.toString()}",
+                                ),
+                                Text(
+                                  "code: ${countryData!.country_code.toString()}",
+                                ),
+                              ],
+                            ),
+                          )
+                          : const SizedBox.shrink(),
+                      SingleChildScrollView(
+                        padding: const EdgeInsets.all(30),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                context.go('/login');
+                              },
+                              child: const Text('Iniciar sesion'),
+                            ),
+                            const SizedBox(height: 16),
+                            ElevatedButton(
+                              onPressed: () async {
+                                setState(() {
+                                  isLoading = true;
+                                });
+                                countryData =
+                                    await IpCountryLookup().getIpLocationData();
+                                setState(() {
+                                  isLoading = false;
+                                  isDataLoaded = true;
+                                });
+                              },
+                              child: const Text('Crear nuevo usuario'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
         ),
       ),
     );
