cb5cc56f13c064acd18eb0c88bd7c2aee40161d5
LilAguh <aguezeochoa@gmail.com>
2025-05-28 19:51:47 -0300

    se declara el bloc de register

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 import 'package:tournament_app/core/get_it_config.dart';
 import 'package:tournament_app/features/auth/config/auth_config.dart';
 import 'package:tournament_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:tournament_app/features/auth/presentation/register/bloc/register_bloc.dart';
 import 'package:tournament_app/features/country/presentation/bloc/country_bloc.dart';
 import 'core/app_route.dart';
 
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         // BlocProvider<AuthBloc>(create: (_) => GetIt.instance<AuthBloc>()),
         BlocProvider(create: (_) => sl<AuthBloc>()),
         BlocProvider(create: (_) => sl<CountryBloc>()),
+        BlocProvider(create: (_) => sl<RegisterBloc>()),
       ],
       child: MaterialApp.router(
         title: 'Tournament APP',
