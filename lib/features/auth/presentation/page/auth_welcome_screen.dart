8653b4a934e39b946c301279f023696b2d254dd1
LilAguh <aguezeochoa@gmail.com>
2025-05-28 04:04:58 -0300

    se llama al sharedpreferences para almacenar el pais, se hace la ruta al register y se borran los textos de debug

--- lib/features/auth/presentation/page/auth_welcome_screen.dart
+++ lib/features/auth/presentation/page/auth_welcome_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tournament_app/core/services/user_location_service.dart';
 import 'package:tournament_app/features/country/presentation/bloc/country_bloc.dart';
-import 'package:tournament_app/features/country/presentation/bloc/country_event.dart';
 import 'package:tournament_app/features/country/presentation/bloc/country_state.dart';
 
 class AuthWelcome extends StatefulWidget {
@@ -27,69 +26,48 @@ class _AuthWelcome extends State<AuthWelcome> {
         backgroundColor: Colors.blue[400],
       ),
       body: Center(
-        child: BlocListener<CountryBloc, CountryState>(
-          listener: (context, state) {
-            if (state is CountrySuccess) {
-              Future.microtask(() {
-                // context.go('/register-method');
-              });
-            }
-          },
-          child:
-              isLoading
-                  ? const CircularProgressIndicator()
-                  : Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (isDataLoaded &&
-                          countryCode != null &&
-                          countryName != null)
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 44),
-                          child: Column(
-                            children: [
-                              Text("País detectado: $countryName"),
-                              Text("Código: $countryCode"),
-                            ],
-                          ),
-                        ),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () => context.go('/login'),
-                        child: const Text('Iniciar sesión'),
-                      ),
-                      const SizedBox(height: 16),
-                      ElevatedButton(
-                        onPressed: () async {
-                          setState(() => isLoading = true);
+        child:
+            isLoading
+                ? const CircularProgressIndicator()
+                : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () => context.go('/login'),
+                      child: const Text('Iniciar sesión'),
+                    ),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () async {
+                        setState(() => isLoading = true);
 
-                          final country =
-                              await UserLocationService()
-                                  .getBestAvailableCountry();
+                        final country =
+                            await UserLocationService()
+                                .getBestAvailableCountry();
 
-                          if (country != null) {
-                            countryCode = country.$1;
-                            countryName = country.$2;
+                        if (country != null) {
+                          countryCode = country.$1;
+                          countryName = country.$2;
 
-                            print('Código: $countryCode');
-                            print('Nombre: $countryName');
-
-                            // Despachás tu evento si hace falta
-                            // context.read<CountryBloc>().add(
-                            //   GetCountryRequested(),
-                            // );
-                          }
-
-                          setState(() {
-                            isLoading = false;
-                            isDataLoaded = true;
+                          await UserLocationService().saveCountryAndCodeToPrefs(
+                            countryCode!,
+                            countryName!,
+                          );
+                          Future.microtask(() {
+                            context.go('/register-method');
                           });
-                        },
-                        child: const Text('Crear nuevo usuario'),
-                      ),
-                    ],
-                  ),
-        ),
+                        }
+
+                        setState(() {
+                          isLoading = false;
+                          isDataLoaded = true;
+                        });
+                      },
+                      child: const Text('Crear nuevo usuario'),
+                    ),
+                  ],
+                ),
       ),
     );
   }
