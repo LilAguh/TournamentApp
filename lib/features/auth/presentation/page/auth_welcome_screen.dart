89770bcae8fa3faecaedc97b882ebff24830f24e
LilAguh <aguezeochoa@gmail.com>
2025-05-28 03:29:51 -0300

    se ejecuta geolocator desde el auth screen

--- lib/features/auth/presentation/page/auth_welcome_screen.dart
+++ lib/features/auth/presentation/page/auth_welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tournament_app/core/services/user_location_service.dart';
 import 'package:tournament_app/features/country/presentation/bloc/country_bloc.dart';
 import 'package:tournament_app/features/country/presentation/bloc/country_event.dart';
 import 'package:tournament_app/features/country/presentation/bloc/country_state.dart';
@@ -13,11 +14,10 @@ class AuthWelcome extends StatefulWidget {
 }
 
 class _AuthWelcome extends State<AuthWelcome> {
-  @override
-  void initState() {
-    super.initState();
-    // context.read<CountryBloc>().add(GetCountryRequested());
-  }
+  bool isDataLoaded = false;
+  bool isLoading = false;
+  String? countryCode;
+  String? countryName;
 
   @override
   Widget build(BuildContext context) {
@@ -26,39 +26,64 @@ class _AuthWelcome extends State<AuthWelcome> {
         title: const Text('auth welcome'),
         backgroundColor: Colors.blue[400],
       ),
-
-      // Centrado
       body: Center(
         child: BlocListener<CountryBloc, CountryState>(
           listener: (context, state) {
             if (state is CountrySuccess) {
               Future.microtask(() {
-                context.go('/register-method');
+                // context.go('/register-method');
               });
             }
           },
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    context.go('/login');
-                  },
-                  child: const Text('Iniciar sesion'),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    final bloc = context.read<CountryBloc>();
-                    bloc.add(GetCountryRequested());
-                  },
-                  child: const Text('Crear nuevo usuario'),
-                ),
-              ],
-            ),
-          ),
+          child:
+              isLoading
+                  ? const CircularProgressIndicator()
+                  : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (isDataLoaded &&
+                          countryCode != null &&
+                          countryName != null)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 44),
+                          child: Column(
+                            children: [
+                              Text("País detectado: $countryName"),
+                              Text("Código: $countryCode"),
+                            ],
+                          ),
+                        ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () => context.go('/login'),
+                        child: const Text('Iniciar sesión'),
+                      ),
+                      const SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () async {
+                          setState(() => isLoading = true);
+
+                          final locationService = UserLocationService();
+                          final data = await locationService.getCountryData();
+
+                          if (data != null) {
+                            countryCode = data.$1;
+                            countryName = data.$2;
+                            // Podés despachar eventos aquí si querés
+                            context.read<CountryBloc>().add(
+                              GetCountryRequested(),
+                            );
+                          }
+
+                          setState(() {
+                            isLoading = false;
+                            isDataLoaded = true;
+                          });
+                        },
+                        child: const Text('Crear nuevo usuario'),
+                      ),
+                    ],
+                  ),
         ),
       ),
     );
