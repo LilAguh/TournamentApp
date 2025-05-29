2712dbebf748d68c9f9f39d2d1e732fe5788b3a3
LilAguh <aguezeochoa@gmail.com>
2025-05-29 02:49:08 -0300

    se arma un widget para un boton custom

---
+++ lib/common/widgets/custom_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback? onPressed;
+  final bool isLoading;
+  final Color? backgroundColor;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    this.onPressed,
+    this.isLoading = false,
+    this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        minimumSize: const Size(double.infinity, 50),
+      ),
+      child:
+          isLoading
+              ? const CircularProgressIndicator(color: Colors.white)
+              : Text(text, style: const TextStyle(fontSize: 16)),
+    );
+  }
+}
