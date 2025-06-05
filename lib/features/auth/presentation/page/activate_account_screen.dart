c267b85cad46361928c3ea9b8de9807756b7079d
LilAguh <aguezeochoa@gmail.com>
2025-06-05 17:15:26 -0300

    se genera la plantilla para el activate

--- lib/features/auth/presentation/page/activate_account_screen.dart
+++ lib/features/auth/presentation/page/activate_account_screen.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tournament_app/common/layout/framed_background.dart';
+import 'package:tournament_app/common/layout/noise_background.dart';
+import 'package:tournament_app/common/widgets/gothic_button.dart';
 
-class ActivateAccount extends StatelessWidget {
-  const ActivateAccount({super.key});
+class ActivateAccountScreen extends StatefulWidget {
+  const ActivateAccountScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('activate account'),
-        backgroundColor: Colors.blue[400],
-      ),
+  State<ActivateAccountScreen> createState() => _ActivateAccountScreenState();
+}
 
-      // Centrado
-      body: Center(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('ACA SE HACE LA AUTORIZACION CON EL CODIGO DEL MAIL'),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  context.go('/new-user-welcome');
-                },
-                child: const Text('Continuar'),
-              ),
-            ],
+class _ActivateAccountScreenState extends State<ActivateAccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return FramedBackground(
+      child: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('ACA SE HACE LA AUTORIZACION CON EL CODIGO DEL MAIL'),
+                const SizedBox(height: 16),
+                GothicButton(
+                  text: 'Continuar',
+                  width:
+                      MediaQuery.of(context).size.width * 0.7, // Ancho relativo
+                  onPressed: () => context.go(''),
+                ),
+              ],
+            ),
           ),
-        ),
+          const Positioned.fill(
+            child: IgnorePointer(
+              child: NoiseBackground(opacity: .05),
+            ), // Reducida opacidad
+          ),
+        ],
       ),
     );
   }
