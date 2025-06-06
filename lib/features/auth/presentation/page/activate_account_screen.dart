853bd995ffd9bcd1c1b2ced7ea92a1b3ec855b67
LilAguh <aguezeochoa@gmail.com>
2025-06-06 06:01:40 -0300

    se integra el funcionamiento basico de pincodefields

--- lib/features/auth/presentation/page/activate_account_screen.dart
+++ lib/features/auth/presentation/page/activate_account_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:tournament_app/common/layout/framed_background.dart';
 import 'package:tournament_app/common/layout/noise_background.dart';
 import 'package:tournament_app/common/widgets/gothic_button.dart';
+import 'package:tournament_app/features/auth/presentation/bloc/auth_bloc.dart';
 
 class ActivateAccountScreen extends StatefulWidget {
   const ActivateAccountScreen({super.key});
@@ -12,6 +15,20 @@ class ActivateAccountScreen extends StatefulWidget {
 }
 
 class _ActivateAccountScreenState extends State<ActivateAccountScreen> {
+  String _code = '';
+
+  void _onConfirmPressed() {
+    if (_code == "12345") {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Código correcto")));
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Código incorrecto")));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return FramedBackground(
@@ -21,21 +38,57 @@ class _ActivateAccountScreenState extends State<ActivateAccountScreen> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Text('ACA SE HACE LA AUTORIZACION CON EL CODIGO DEL MAIL'),
-                const SizedBox(height: 16),
+                const Text(
+                  'Registro',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFFD6C8B0),
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Classicloud',
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Text(
+                  'Ingresá el código enviado por correo',
+                  style: TextStyle(fontSize: 20, color: Color(0xFFD6C8B0)),
+                ),
+                const SizedBox(height: 24),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: PinCodeTextField(
+                    appContext: context,
+                    length: 5,
+                    onChanged: (value) => setState(() => _code = value),
+                    keyboardType: TextInputType.number,
+                    autoFocus: true,
+                    animationType: AnimationType.fade,
+                    pinTheme: PinTheme(
+                      shape: PinCodeFieldShape.box,
+                      borderRadius: BorderRadius.circular(12),
+                      fieldHeight: 52,
+                      fieldWidth: 45,
+                      activeFillColor: Colors.white,
+                      inactiveFillColor: Colors.white,
+                      selectedFillColor: Colors.grey[100],
+                      inactiveColor: Colors.grey[400]!,
+                      activeColor: Colors.blueAccent,
+                      selectedColor: Colors.blue,
+                    ),
+                    enableActiveFill: true,
+                  ),
+                ),
+                const SizedBox(height: 24),
                 GothicButton(
-                  text: 'Continuar',
-                  width:
-                      MediaQuery.of(context).size.width * 0.7, // Ancho relativo
-                  onPressed: () => context.go(''),
+                  text: 'Activar cuenta',
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  onPressed: _code.length == 5 ? _onConfirmPressed : null,
                 ),
               ],
             ),
           ),
           const Positioned.fill(
-            child: IgnorePointer(
-              child: NoiseBackground(opacity: .05),
-            ), // Reducida opacidad
+            child: IgnorePointer(child: NoiseBackground(opacity: .05)),
           ),
         ],
       ),
